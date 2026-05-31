683a0a04264246058c87682ea88fe259fc0d4808
GreyF <greysforgettea@gmail.com>
2026-05-31 23:19:05 +0700

    font added

--- app/src/main/java/com/example/workup/ui/theme/Type.kt
+++ app/src/main/java/com/example/workup/ui/theme/Type.kt
@@ -1,34 +1,18 @@
 package com.example.workup.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.workup.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val OxaniumFontFamily = FontFamily(
+    Font(R.font.oxanium_regular, FontWeight.Normal),
+    Font(R.font.oxanium_medium, FontWeight.Medium),
+    Font(R.font.oxanium_bold, FontWeight.Bold)
+)
+
+
+val AppTypography = Typography(
+    //
 )
