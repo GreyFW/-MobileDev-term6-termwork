cedc00617cd6c9e2740d7431c4414ee168b7041d
GreyF <greysforgettea@gmail.com>
2026-05-31 19:49:10 +0700

    Entities started

---
+++ domain/src/main/java/com/example/domain/Exercise.kt
@@ -0,0 +1,16 @@
+package com.example.domain
+
+enum class ExerciseType {
+    DB,  // Dumbbell
+    BB,  // Barbell
+    FW,  // Free Weight
+    P    // Plate
+}
+
+data class Exercise(
+    val id: String,
+    val name: String,
+    val type: ExerciseType,
+    val weight: Float,
+    val repsCount: List<Int>
+)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "Work UP"
 include(":app")
+include(":domain")
+include(":data")
